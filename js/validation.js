b7057cca1fea4c89e843aa862525918bff768811
Matthew Chamberlain <matthewwillliamchamberlain@gmail.com>
2016-04-13 10:01:50 +1200

    Start validation code

---
+++ js/validation.js
@@ -0,0 +1,10 @@
+//find the form
+var resgistrationForm = document.querySelector('#registration');
+
+//wait for the user to submit the form
+resgistrationForm.onsubmit = function(event) {
+
+	// Stop the form from submitting
+	event.preventDefault();
+	
+}
